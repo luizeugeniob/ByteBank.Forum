a33c340ebb3fc9872e703cbc34160e62f7de7bb4
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-12-08 23:25:08 -0300

    Criação da AccountController e página de registro de usuário.

---
+++ ByteBank.Forum/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Forum.ViewModels;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Register(AccountRegisterViewModel accountRegisterViewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ ByteBank.Forum/ViewModels/AccountRegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class AccountRegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Nome completo")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
