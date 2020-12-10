08f64b8ef98a5c8ec66f44032e1aeb197fecd561
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-12-09 23:31:41 -0300

    Implementação de configuração do owin.

--- ByteBank.Forum/Controllers/AccountController.cs
+++ ByteBank.Forum/Controllers/AccountController.cs
@@ -1,28 +1,43 @@
 ﻿using ByteBank.Forum.Models;
 using ByteBank.Forum.ViewModels;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace ByteBank.Forum.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+        public UserManager<ApplicationUser> UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    _userManager = contextOwin.GetUserManager<UserManager<ApplicationUser>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Register()
         {
             return View();
         }
-        
+
         [HttpPost]
         public async Task<ActionResult> Register(AccountRegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var dbContext = new IdentityDbContext<ApplicationUser>("DefaultConnection");
-                var userStore = new UserStore<ApplicationUser>(dbContext);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-
                 var newUser = new ApplicationUser
                 {
                     Email = model.Email,
@@ -30,7 +45,7 @@ namespace ByteBank.Forum.Controllers
                     FullName = model.FullName
                 };
 
-                await userManager.CreateAsync(newUser, model.Password);
+                await _userManager.CreateAsync(newUser, model.Password);
 
                 return RedirectToAction("Index", "Home");
             }
---
+++ ByteBank.Forum/Startup.cs
@@ -0,0 +1,35 @@
+﻿using ByteBank.Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartup(typeof(ByteBank.Forum.Startup))]
+
+namespace ByteBank.Forum
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() =>
+                new IdentityDbContext<ApplicationUser>("DefaultConnection"));
+
+            builder.CreatePerOwinContext<IUserStore<ApplicationUser>>(
+            (opcoes, contextoOwin) =>
+            {
+                var dbContext = contextoOwin.Get<DbContext>();
+                return new UserStore<ApplicationUser>(dbContext);
+            });
+
+            builder.CreatePerOwinContext<UserManager<ApplicationUser>>(
+            (opcoes, contextoOwin) =>
+            {
+                var userStore = contextoOwin.Get<IUserStore<ApplicationUser>>();
+                return new UserManager<ApplicationUser>(userStore);
+            });
+        }
+    }
+}
